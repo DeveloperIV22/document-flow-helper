c179520ce51373b5e971817c02f1512714f3a7ff
Victor <victor@victor.com>
2025-03-08 13:25:35 +0200

    removed eslint

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  const unusedVariable = 'Thssis will cause a error'
+  const tt = 3
+  return (
+    <div>
+      <button className="btn btn-primary">dashbaord</button>
+    </div>
+  )
+}
