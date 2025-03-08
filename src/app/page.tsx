b3a194cf1b6332e8a8be479c179b41188c6015f9
Victor <victor@victor.com>
2025-03-08 12:41:46 +0200

    finis eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 
 export default function Home() {
+  const unusedVariable = 'This will cause a warning'
+
   return (
     <div>
       <button className="btn btn-primary">Button</button>
